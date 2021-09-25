2dfdc97aea8333c41d4c1b23228cfa97091391ed
smokyweb <jamtechba@gmail.com>
2021-09-25 14:37:17 +0530

    host added

--- index.php
+++ index.php
@@ -1,49 +1,12 @@
 <?php
 
-$host = 'real258.herokuapp.com';
-$ports = array(21, 25, 80, 81, 110, 443, 3306);
-
-foreach ($ports as $port)
+<?php
+if(fsockopen("real258.herokuapp.com",80))
 {
-    $connection = @fsockopen($host, $port);
-
-    if (is_resource($connection))
-    {
-        echo '<h2>' . $host . ':' . $port . ' ' . '(' . getservbyport($port, 'tcp') . ') is open.</h2>' . "\n";
-
-        fclose($connection);
-    }
-
-    else
-    {
-        echo '<h2>' . $host . ':' . $port . ' is not responding.</h2>' . "\n";
-    }
+print "I can see port 80";
 }
-
-// set some variables
-/*$host = "real258.herokuapp.com";
-$port = ;
-// don't timeout!
-set_time_limit(0);
-// create socket
-$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-// bind socket to port
-$result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
-// start listening for connections
-$result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
-
-// accept incoming connections
-// spawn another socket to handle communication
-$spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
-// read client input
-$input = socket_read($spawn, 1024) or die("Could not read input\n");
-// clean up input string
-$input = trim($input);
-echo "Client Message : ".$input;
-// reverse client input and send back
-$output = strrev($input) . "\n";
-socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
-// close sockets
-socket_close($spawn);
-socket_close($socket);*/
+else
+{
+print "I cannot see port 80";
+}
 ?>
